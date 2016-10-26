41b148364438fa4fdc9bde3cc866d60e64c2a4d2
Brad Efting <bradefting@gmail.com>
2016-10-26 15:38:17 -0600

    working site

--- script.js
+++ script.js
@@ -6,46 +6,45 @@ var subtotal=0;
 
   $('.burger').on('click', function(){
     $('.items').append("<tr><td>" + " Royale with Cheese " + "</td><td class='right-align'> " + " $8.99" + "</td></tr>");
-    subtotalFunc(8.99)
+    subtotalFunc(8.99);
   });
 
   $('.pizza').on('click', function(){
     $('.items').append("<tr><td>" + " Arugula Pizza " + "</td><td class='right-align'> " + " $11.99" + "</td></tr>");
-    subtotalFunc(11.99)
+    subtotalFunc(11.99);
   });
 
   $('.ribs').on('click', function(){
     $('.items').append("<tr><td>" + " Smoked Ribs " + "</td><td class='right-align'> " + " $14.99" + "</td></tr>");
-    subtotalFunc(14.99)
+    subtotalFunc(14.99);
   });
 
   $('.biscuit').on('click', function(){
     $('.items').append("<tr><td>" + " Ice Cream Biscuit " + "</td><td class='right-align'> " + " $7.99" + "</td></tr>");
-    subtotalFunc(7.99)
+    subtotalFunc(7.99);
   });
 
   var subtotalFunc = function(itemAmount){
-    // $(".cart").removeClass('hidden');
-    subtotal+=itemAmount
+    subtotal+=itemAmount;
     subtotal = parseFloat((Math.round(subtotal * 100) /100).toFixed(2));
     $('.subtotalAmount').text('$' + subtotal);
     taxFunc(subtotal);
-  }
+  };
 
   var taxFunc = function(subtotal){
-    var totalTax = (subtotal * .06);
+    var totalTax = (subtotal * 0.06);
     totalTax = parseFloat((Math.round(totalTax * 100) /100).toFixed(2));
     console.log(typeof totalTax, " totalTax after round");
     $('.taxAmount').text('$' + totalTax);
     orderTotal(subtotal, totalTax);
-  }
+  };
 
   var orderTotal = function(subtotal, totalTax){
     var total = (subtotal + totalTax);
     total = (Math.round(total * 100) /100).toFixed(2);
-    // console.log(total);
+    //total is a string at this point
     $('.totalAmount').text('$' + total);
-  }
+  };
 
   $('#submitForm').on('click', function(e){
     e.preventDefault();
@@ -56,14 +55,12 @@ var subtotal=0;
     if(subtotal>0){
 
       if(name && phone && address){
-        // console.log(name);
-        // console.log(phone);
-        // console.log(address);
+        Materialize.toast("Thank you for your order.", 4000);
       }else{
-        Materialize.toast("You're missing some required information, please input your name, phone number and address.", 4000)
+        Materialize.toast("You're missing some required information, please verify that your name, phone number, and address were entered correctly.", 5000);
       }
     }else{
-      Materialize.toast("There is nothing in your cart. Please add an item to the cart.", 4000)
+      Materialize.toast("There are no items in your cart. Please add items to the cart before submitting your order.", 5000);
     }
   });
 
