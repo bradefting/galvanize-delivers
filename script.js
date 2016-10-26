caa20858172c681a8c65ddb5886826b083fec2fb
Brad Efting <bradefting@gmail.com>
2016-10-26 09:02:58 -0600

    add toast

--- script.js
+++ script.js
@@ -47,11 +47,27 @@ var subtotal=0;
     $('.totalAmount').text('$' + total);
   }
 
-  $('#submitForm').on('click', function(){
+  $('#submitForm').on('click', function(e){
+    e.preventDefault();
     var name= $('#customerName[name="customerName"]').val();
     var phone=$('#customerPhone[name="customerPhone"]').val();
-    var phone=$('#customerPhone[name="customerPhone"]').val();
-    console.log(name + " " + phone);
+    var address=$('#address[name="address"]').val();
+
+    if(subtotal>0){
+
+      if(name && phone && address){
+        // console.log(name);
+        // console.log(phone);
+        // console.log(address);
+      }else{
+        Materialize.toast("You're missing some required information, please input your name, phone number and address.", 4000)
+      }
+    }else{
+      Materialize.toast("There is nothing in your cart. Please add an item to the cart.", 4000)
+    }
   });
+
+  // initialize nav bar
+  $(".button-collapse").sideNav();
 
 }); // end of document ready
